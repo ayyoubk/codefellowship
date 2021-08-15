0de7c8f284209f97a97bc81d425f79e995eab090
ayyoubk <ayoub.alqeam@gmail.com>
2021-08-15 22:45:38 +0300

    almost done

---
+++ src/main/java/com/lab/codefellowship/controllers/ApplicationUserController.java
@@ -0,0 +1,48 @@
+package com.lab.codefellowship.controllers;
+
+import com.lab.codefellowship.models.ApplicationUser;
+import com.lab.codefellowship.repositories.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ApplicationUserController {
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/signup")
+    public String getSignUpPage() {
+        return "signup.html";
+    }
+
+    @GetMapping("/login")
+    public String getSignInPage() {
+        return "signin.html";
+    }
+
+    @PostMapping("/signup")
+    public RedirectView signUp(@RequestParam(value = "username") String username,
+                               @RequestParam(value = "password") String password,
+                               @RequestParam(value = "firstName") String firstName,
+                               @RequestParam(value = "lastName") String lastName,
+                               @RequestParam(value = "dateOfBirth") String dateOfBirth,
+                               @RequestParam(value = "bio") String bio
+    ) {
+        ApplicationUser newUser = new ApplicationUser(username, bCryptPasswordEncoder.encode(password), firstName, lastName, dateOfBirth, bio);
+        applicationUserRepository.save(newUser);
+        return new RedirectView("/login");
+    }
+    @GetMapping("/users/{id}")
+    public String getProfile(@PathVariable("id") Integer id, Model m) {
+        ApplicationUser user = applicationUserRepository.findById(id).get();
+        m.addAttribute("user", user);
+        return "profile.html";
+    }
+
+}
